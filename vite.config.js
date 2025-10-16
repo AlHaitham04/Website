238be7bee927dc70cdbaa4fbf4f571ae3f9fceaa
unknown <haitham8213@gmail.com>
2025-10-16 17:52:42 +0400

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/website/',
+  base: '/website/', // ← must match the repo name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      }
-    }
-  }
 })
